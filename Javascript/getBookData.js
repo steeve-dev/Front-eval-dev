f0611a20b90b220a36c97c47a94c80c3a75c2173
steeve mellet <melletsteeve@orange.fr>
2022-10-05 19:44:46 +0200

    initial commit

---
+++ Javascript/getBookData.js
@@ -0,0 +1,22 @@
+fetch('http://127.0.0.1:8000/api/books.json')
+  .then(response => {
+    if (response.ok){
+      response.json().then(data => {
+        
+      for(let book of data){
+        document.getElementById('ISBNBook').innerHTML = book.ISBN
+        document.getElementById('titleBook').innerHTML = book.title
+        document.getElementById('authorBook').innerHTML = book.authors.Name
+        document.querySelector('#BookType').innerHTML = book.type.name
+        document.querySelector('#BookDescription').innerHTML = book.description
+        document.querySelector('#BookPublishDate').innerHTML = book.PublishedDate
+        document.querySelector('#BookEditor').innerHTML = book.editor.name
+    }
+
+        console.log(data)
+    })
+    } else {
+      console.log('error')
+    }
+  })
+  
